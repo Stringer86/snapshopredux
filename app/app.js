623795675795565a55c9915f6f833ec9b7386524
Stringer86 <stringer.stephen@gmail.com>
2016-11-03 12:36:11 -0700

    late commit

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import angularMaterialize from 'angular-materialize';
+import CatalogCtrl from './catalog/catalog.controller';
+
+
+angular.module('my-app', [angularMaterialize])
+  .controller('CatalogCtrl', CatalogCtrl);
