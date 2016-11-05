9bc540609859a371bf6106965fb351b2a20da7d5
Stringer86 <stringer.stephen@gmail.com>
2016-11-05 12:09:38 -0700

    ui_router branch made before cart_service merge

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router'
 import angularMaterialize from 'angular-materialize';
 import CatalogCtrl from './catalog/catalog.controller';
 import CartCtrl from './cart/cart.controller';
@@ -7,8 +8,20 @@ import CartCountCtrl from './nav/cart_count.controller.js';
 import CatalogService from './catalog/catalog.services';
 
 
-angular.module('my-app', [angularMaterialize])
+angular.module('my-app', [angularMaterialize, uiRouter])
   .service('CatalogService', CatalogService)
   .controller('CatalogCtrl', CatalogCtrl)
   .controller('CartCtrl', CartCtrl)
   .controller('CartCountCtrl', CartCountCtrl)
+
+  .config(['$stateProvider', ($stateProvider) => {
+    $stateProvider
+      .state('home', {
+        url: '',
+        templateUrl: 'views/home.html'
+      })
+      .state('main', {
+        url: '/',
+        templateUrl: 'views/home.html'
+      })
+  }]);
