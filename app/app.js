1fd30512d012f4533d4944be4db98f0baa2a1e14
Stringer86 <stringer.stephen@gmail.com>
2016-11-09 12:21:34 -0800

    signup controller and service working

--- app/app.js
+++ app/app.js
@@ -6,20 +6,24 @@ import CatalogCtrl from './catalog/catalog.controller';
 import CartCtrl from './cart/cart.controller';
 import CartCountCtrl from './nav/cart_count.controller';
 import OrdersCtrl from './orders/orders.controller';
+import SignUpCtrl from './signup/signup.controller';
 
 import CatalogService from './catalog/catalog.services';
 import CartService from './cart/cart.services';
 import OrdersService from './orders/orders.services';
+import SignUpService from './signup/signup.services';
 
 
 angular.module('my-app', [angularMaterialize, uiRouter])
   .service('CatalogService', CatalogService)
   .service('CartService', CartService)
   .service('OrdersService', OrdersService)
+  .service('SignUpService', SignUpService)
   .controller('CatalogCtrl', CatalogCtrl)
   .controller('CartCtrl', CartCtrl)
   .controller('CartCountCtrl', CartCountCtrl)
   .controller('OrdersCtrl', OrdersCtrl)
+  .controller('SignUpCtrl', SignUpCtrl)
   .config(['$stateProvider', ($stateProvider) => {
     $stateProvider
       .state('home', {
