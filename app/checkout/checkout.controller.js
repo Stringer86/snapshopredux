1238ef02355a3eb601cfe64a6bc66e9bde9c398a
Stringer86 <stringer.stephen@gmail.com>
2016-11-11 10:00:09 -0800

    checkout and cart service combined

--- app/checkout/checkout.controller.js
+++ app/checkout/checkout.controller.js
@@ -13,17 +13,6 @@ class CheckoutCtrl {
 
   purchase(items, first, last, address1, address2, city, state, zip) {
     this.checkoutSvc.purchase(items, this.first, this.last, this.address1, this.address2, this.city, this.state, this.zip)
-    .then((e) => {
-      this.cartSvc.cart.length = 0;
-      this.cartSvc.subtotal = 0;
-      this.first = '';
-      this.last = '';
-      this.address1 = '';
-      this.address2 = '';
-      this.city = '';
-      this.state = '';
-      this.zip = '';
-    })
   }
 
 
--- app/checkout/checkout.services.js
+++ app/checkout/checkout.services.js
@@ -1,12 +1,15 @@
 class CheckoutService {
-  constructor($http) {
+  constructor($http, cartSvc) {
     this.$http = $http;
+    this.cartSvc = cartSvc;
 
 }
 purchase(items, first, last, address1, address2, city, state, zip) {
     return this.$http.post('/orders', { items, address1, address2, city, state, zip })
       .then((response) => {
       Materialize.toast('Order Complete!', 4000, 'blue rounded');
+      this.cartSvc.cart.length = 0;
+      this.cartSvc.subtotal = 0;
       return response.data;
       })
       .catch((err) => {
@@ -15,5 +18,6 @@ purchase(items, first, last, address1, address2, city, state, zip) {
   }
 };
 
+CheckoutService.$inject = ['$http', 'CartService'];
 
 export default CheckoutService;
